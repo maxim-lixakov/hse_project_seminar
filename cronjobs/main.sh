d3bfe4bef63db654c998ba54ebc96b7f7a1480b9
root <root@865329-cl82032.tmweb.ru>
2022-05-09 22:38:34 +0300

    changed paths

--- cronjobs/main.sh
+++ cronjobs/main.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 # activate venv
-. "/home/mvliksakov/project_seminar/hse_project_seminar/venv/bin/activate"
+. "/home/mvliksakov/project_seminar/hse_project_seminar/crawling/venv/bin/activate"
 #go to spider directory
-cd  "/home/mvliksakov/project_seminar/hse_project_seminar/wildberries/wildberries/spiders"
+cd  "/home/mvliksakov/project_seminar/hse_project_seminar/crawling/wildberries/wildberries/spiders"
 now=$(date +"%m_%d_%Y")
 echo $now
 #run scrapy spider
-python "/home/mvliksakov/project_seminar/hse_project_seminar/test.py"
-scrapy crawl goods --logfile log.log -o res_for_$now.jl
+scrapy crawl goods -o res_for_$now.jl
 
