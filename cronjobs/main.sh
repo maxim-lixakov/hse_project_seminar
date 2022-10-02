6bf579767de2f8489d6c6f3ba98be2fe1e0c176f
root <root@1046685-cl82032.tmweb.ru>
2022-10-02 15:45:51 +0300

    new_server

--- cronjobs/main.sh
+++ cronjobs/main.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # activate venv
-. "/home/mvliksakov/project_seminar/hse_project_seminar/crawling/venv/bin/activate"
+. "/root/hse_project_seminar/crawling/venv/bin/activate"
 #go to spider directory
-cd  "/home/mvliksakov/project_seminar/hse_project_seminar/crawling/wildberries/wildberries/spiders"
+cd  "/root/hse_project_seminar/crawling/wildberries/wildberries/spiders"
 now=$(date +"%m_%d_%Y")
 echo $now
 #run scrapy spider
 scrapy crawl goods -o res_for_$now.jl
 #insert to psql
-python /home/mvliksakov/project_seminar/hse_project_seminar/jl_to_psql/main.py
+python /root/hse_project_seminar/jl_to_psql/main.py
 
