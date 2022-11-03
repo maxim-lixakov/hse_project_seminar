f992f9aa8373a39b6d13366164ee8d7a98d55afd
lixak <lixakov.makim@yandex.ru>
2022-11-03 19:42:36 +0300

    created welcome page

--- service/services/frontend/src/App.js
+++ service/services/frontend/src/App.js
@@ -1,22 +1,56 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>My SHIT</p>
-      </header>
-    </div>
-  );
+import React from "react";
+import './App.css';
+import Circle from 'react-circle';
+
+
+
+class App extends React.Component {
+
+    // Constructor
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: [],
+            DataisLoaded: false
+        };
+    }
+
+    // ComponentDidMount is used to
+    // execute the code
+    componentDidMount() {
+        fetch(
+"http://localhost:1337/api/data_info")
+            .then((res) => res.json())
+            .then((json) => {
+                this.setState({
+                    items: json,
+                    DataisLoaded: true
+                });
+            })
+    }
+    render() {
+          const myStyle={
+                backgroundImage: "url(/img/image.png)",
+                height:'100vh',
+                fontSize:'50px',
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+            };
+          const { DataisLoaded, items } = this.state;
+        if (!DataisLoaded) return <div>
+            <h1> Please wait some time.... </h1> </div> ;
+          return (
+        <div style={myStyle} >
+
+<Circle
+    size={170}
+    showPercentageSymbol={false}
+  progress={items.rows}
+/>
+        </div>
+          );
+        }
 }
+
 export default App;
